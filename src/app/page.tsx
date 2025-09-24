34a9264325a20bb8cc66d7550f16ca2c0d03f6f9
Daviies <106864737+Daviies@users.noreply.github.com>
2025-09-24 08:02:51 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 // import Image from "next/image";
-// import Image from "next/image";
-// import Image from "next/image";
+
+import type { NextConfig } from "next";
+            layoutDirection === "LR" ? "-mt-9 mr-14" : "mb-14 -ml-6",
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
 
 export default function Home() {
   return (
